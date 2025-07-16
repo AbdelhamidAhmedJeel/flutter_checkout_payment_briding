62378cf77a66c49899ec1e6473932b68006718f6
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-16 15:41:34 +0300

    adding Json Dependencies

--- flutter_checkout_payments_briding/android/app/build.gradle.kts
+++ flutter_checkout_payments_briding/android/app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies{
     implementation("anroidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
 
+    // Json proccessing
+    implementation("com.google.code.gson:gson:2.13.1")
+
 
 
 
