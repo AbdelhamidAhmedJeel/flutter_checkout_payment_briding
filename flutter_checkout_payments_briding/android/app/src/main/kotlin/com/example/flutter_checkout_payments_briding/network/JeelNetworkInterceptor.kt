f6144ee375877e0c99da189a36096c470eaaa566
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-17 13:37:18 +0300

    Inject network interceptor constructor

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/JeelNetworkInterceptor.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/JeelNetworkInterceptor.kt
@@ -1,6 +1,7 @@
 package com.example.flutter_checkout_payments_briding.network
 
 import android.content.Context
+import jakarta.inject.Inject
 
 class JeelNetworkInterceptor @Inject constructor(context: Context){
 }
