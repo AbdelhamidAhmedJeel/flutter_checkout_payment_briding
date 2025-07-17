0729b71a40673b804ec2115476547ea7ad0b55f7
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-17 19:50:38 +0300

    add request body as parameter

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/PaymentsRequestsInterface.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/PaymentsRequestsInterface.kt
@@ -1,10 +1,12 @@
 package com.example.flutter_checkout_payments_briding.network
 
 import com.example.flutter_checkout_payments_briding.utils.JeelServerResponse
+import okhttp3.RequestBody
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface PaymentsRequestsInterface {
     @POST("payments/orders")
-    suspend fun sendOrderInformation(): Response<JeelServerResponse>
+    suspend fun sendOrderInformation(@Body body: RequestBody): Response<JeelServerResponse>
 }
