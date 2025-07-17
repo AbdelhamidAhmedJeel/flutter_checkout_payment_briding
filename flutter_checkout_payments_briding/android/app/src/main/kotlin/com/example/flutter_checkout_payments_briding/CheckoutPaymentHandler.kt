0c544b34a1f55ace8de86639279c1b14e6729fea
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-17 18:40:23 +0300

    update checkout payment handler to create method handler || this class will be romoved for code enhancements

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/CheckoutPaymentHandler.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/CheckoutPaymentHandler.kt
@@ -1,17 +1,40 @@
 package com.example.flutter_checkout_payments_briding
-import com.example.flutter_checkout_payments_briding.MainActivity
+import androidx.compose.runtime.key
 import com.google.gson.Gson
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import okhttp3.Dispatcher
 
 class CheckoutPaymentHandler(
     private val activity: MainActivity
 ){
-    private val publicKey:String? = null;
+
+    private var publicKey:String? = null;
     private val baseUrl: String = "https://api.sandbox.checkout.com"
     private val  scope = CoroutineScope(Dispatchers.IO+ SupervisorJob())
     private  val  gson = Gson()
 
+    fun handleMethodCall(call: MethodCall, result: MethodChannel.Result){
+        when(call.method){
+            "initialize" -> initialize(call,result)
+        }
+    }
+
+  private  fun initialize(call: MethodCall,result: MethodChannel.Result){
+        try{
+            val incomingKey = call.argument<String>("publicKey")
+            val isProduction = call.argument<Boolean>("isProduction")
+            if(incomingKey ==null){
+                result.error("Invalid Arguments","public key is required",null)
+                return
+            }
+            publicKey = incomingKey
+            result.success(true)
+
+        }catch (e :Exception){
+            result.error("Initialization error", e.message,null)
+        }
+    }
 }
