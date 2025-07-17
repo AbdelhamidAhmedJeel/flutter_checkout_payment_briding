0171b78050edd67c08917aeffd8080dc58e78045
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-17 18:40:49 +0300

    initializing method channel in the main activity

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/MainActivity.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.example.flutter_checkout_payments_briding
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+
+
+
+class MainActivity : FlutterActivity(){
+    private val  channel = "com.example.flutter_checkout_payments_briding/checkout"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channel
+        ).setMethodCallHandler { call, result ->
+            val  checkoutPaymentHandler = CheckoutPaymentHandler(this)
+            checkoutPaymentHandler.handleMethodCall(call,result)
+
+    }
+        }
+
+}
