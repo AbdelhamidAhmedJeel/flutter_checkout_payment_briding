d6855eb368794b7d1d7f426e8c104d6960bd6106
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-17 19:37:26 +0300

    create safe api request

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/SafeApiRequest.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/SafeApiRequest.kt
@@ -1,9 +1,33 @@
 package com.example.flutter_checkout_payments_briding.network
 
+import com.example.flutter_checkout_payments_briding.utils.ApiExceptions
+import com.example.flutter_checkout_payments_briding.utils.JeelServerResponse
+import com.google.gson.Gson
 import retrofit2.Response
 
 abstract class SafeApiRequest {
-    suspend fun <T: Any> apiRequest(call: suspend () -> Response<T>): T {
 
+    suspend fun <T : Any> apiRequest(call: suspend () -> Response<T>): T {
+        val response = call.invoke()
+
+        if (response.isSuccessful) {
+            return response.body() ?: throw ApiExceptions("Empty response body")
+        } else {
+            val errorBody = response.errorBody()?.string()
+            val message = StringBuilder()
+
+            try {
+                val errorResponse = Gson().fromJson(errorBody, JeelServerResponse::class.java)
+                errorResponse.errors?.let {
+                    it.forEach { error ->
+                        message.append("${error.description}\n")
+                    }
+                }  ?: message.append("Unknown error occurred.")
+            } catch (e: Exception) {
+                message.append("Error parsing error response")
+            }
+
+            throw ApiExceptions(message.toString())
+        }
     }
 }
---
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/utils/JeelServerResponse.kt
@@ -0,0 +1,10 @@
+package com.example.flutter_checkout_payments_briding.utils
+
+import android.os.Message
+import com.google.gson.annotations.SerializedName
+
+data  class JeelServerResponse(@SerializedName ("content") var responseData:Any? =null
+,@SerializedName("errors") var errors: List<JeelServerErrors>? =null)
+
+data class JeelServerErrors(@SerializedName("id") val id: String? =null,@SerializedName("messgae") val message: String? = null,
+    @SerializedName("description") val description:String? = null)
