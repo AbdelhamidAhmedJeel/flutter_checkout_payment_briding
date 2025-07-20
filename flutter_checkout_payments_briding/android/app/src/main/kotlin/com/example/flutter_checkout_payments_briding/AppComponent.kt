7be193d1f969f5220ecc6bf267dc88016aae590e
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-20 12:01:16 +0300

    add getCheckoutPaymentHandler in app component

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/AppComponent.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/AppComponent.kt
@@ -1,12 +1,18 @@
 package com.example.flutter_checkout_payments_briding
 
 import com.example.flutter_checkout_payments_briding.modules.JeelApplicationModule
+import com.example.flutter_checkout_payments_briding.network.RetrofitApi
+import com.example.flutter_checkout_payments_briding.repositories.JeelPaymentsRepository
 import dagger.Component
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 
 @Singleton
-@Component(modules = [JeelApplicationModule::class])
+@Component(modules = [RetrofitApi::class, JeelApplicationModule::class])
 interface AppComponent {
     fun inject(mainActivity: MainActivity)
+
+    fun getCheckoutPaymentHandler(): CheckoutPaymentHandler
+
 }
