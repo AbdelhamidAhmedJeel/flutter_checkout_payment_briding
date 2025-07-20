8a26e5ec924a409b1b3efca7ef98a50f3ee11a47
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-20 12:05:53 +0300

    refactor api exceptions

---
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/exceptions/ApiExceptions.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_checkout_payments_briding.network.exceptions
+
+import java.io.IOException
+
+class ApiExceptions (message:String?): IOException(message)
+class  NetworkExceptions (message:String): IOException(message)
--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/utils/ApiExceptions.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_checkout_payments_briding.utils
-
-import java.io.IOException
-
-class ApiExceptions (message:String?): IOException (message)
-class  NetworkExceptions (message:String): IOException (message)
