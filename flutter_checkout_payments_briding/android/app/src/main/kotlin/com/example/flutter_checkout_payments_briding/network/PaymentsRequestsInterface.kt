393f90ae367ff37db4a1e34a706c64dd20c18ad8
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-20 12:06:32 +0300

    update payment request interface

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/PaymentsRequestsInterface.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/PaymentsRequestsInterface.kt
@@ -1,12 +1,17 @@
 package com.example.flutter_checkout_payments_briding.network
 
-import com.example.flutter_checkout_payments_briding.utils.JeelServerResponse
+import com.example.flutter_checkout_payments_briding.models.request_parameters.PaymentSessionParams
+import com.example.flutter_checkout_payments_briding.network.responses.JeelServerResponse
+import com.example.flutter_checkout_payments_briding.network.responses.PaymentSessionResponse
 import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.HeaderMap
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface PaymentsRequestsInterface {
-    @POST("payments/orders")
-    suspend fun sendOrderInformation(@Body body: RequestBody): Response<JeelServerResponse>
+
+    @POST("payment-sessions")
+    suspend fun createPaymentSession(@HeaderMap myHeaders: Map<String, String>, @Body body: PaymentSessionParams): Response<PaymentSessionResponse>
 }
