76880122532dd3bc869435bdc088f6c53c6b2925
Abdelhamid Ahmed <a.ahmed.c@jeel.com>
2025-07-20 11:59:51 +0300

    update retrofit api

--- flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/RetrofitApi.kt
+++ flutter_checkout_payments_briding/android/app/src/main/kotlin/com/example/flutter_checkout_payments_briding/network/RetrofitApi.kt
@@ -1,7 +1,8 @@
 package com.example.flutter_checkout_payments_briding.network
 
+import android.util.Log
+import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
-import com.suecal.suecalcrmnewkmm.data.network.JeelNetworkInterceptor
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -11,27 +12,41 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
+private const val TAG = "RetrofitApi"
 @Module
 class RetrofitApi @Inject constructor(private var networkInterceptor: JeelNetworkInterceptor) {
-    private var retrofit: Retrofit? = null
+
+    private lateinit var retrofit: Retrofit
 
     @Provides
     @Singleton
-    fun  getRetrofitApiSandbox(): Retrofit?{
-        if(retrofit ==null){
-            val gson = GsonBuilder().create()
-            retrofit = Retrofit.Builder().baseUrl("https://").client(OkHttpClient.Builder().readTimeout(100,
-                TimeUnit.SECONDS).connectTimeout(100, TimeUnit.SECONDS)
-                .addInterceptor(networkInterceptor).build()).addConverterFactory(
-                GsonConverterFactory.create(gson)).build()
+    fun provideRetrofit(): Retrofit {
+        if (!::retrofit.isInitialized) {
+
+
+            val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+
+            retrofit = Retrofit.Builder()
+                .baseUrl("https://api.sandbox.checkout.com/")
+                .client(
+                    OkHttpClient.Builder()
+                        .readTimeout(100, TimeUnit.SECONDS)
+                        .connectTimeout(100, TimeUnit.SECONDS)
+                        .addInterceptor(networkInterceptor)
+                        .build()
+                )
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
         }
+
         return retrofit
     }
 
     @Provides
     @Singleton
-    fun getPaymentsRequestInterface(): PaymentsRequestsInterface{
-        return getRetrofitApiSandbox()!!.create(PaymentsRequestsInterface::class.java)
-
+    fun providePaymentsRequestInterface(): PaymentsRequestsInterface {
+        return provideRetrofit().create(PaymentsRequestsInterface::class.java)
     }
 }
